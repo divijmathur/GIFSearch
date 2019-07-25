ba1481eeb7230c22d06acd5d3fdba833fe4cc7a8
Mona Lisa <divijmathur@gmail.com>
2019-07-24 22:38:04 -0700

    added searchability function of gifs

--- gif.js
+++ gif.js
@@ -12,3 +12,22 @@ function renderButtons() {
 }
 
 renderButtons();
+
+$("#add-giphy").on('click',function() {
+        event.preventDefault();
+        var gifVal = $("#giphy-input").val();
+        var u = "https://api.giphy.com/v1/gifs/search?api_key=sKYmnUIjhERlGh0oJCv82J2pKvVCfeeU&q=" + gifVal + "&limit=10&offset=0&rating=G&lang=en";
+        console.log(u);
+        $.ajax({
+            url: u,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+            for (var i = 0; i < response.data.length; i++) {
+                var a = $("<img>");
+                a.attr('src', response.data[i].images.downsized_large.url);
+                $('div').append(a);
+            }
+            debugger;
+        });
+});
